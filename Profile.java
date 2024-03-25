15b1aa5923b4ea3151779eebfb620945e5585395
jirachixu <xu1838@purdue.edu>
2024-03-24 23:31:58 -0400

    completed basic Profile class, see TODO for further implementation

--- Profile.java
+++ Profile.java
@@ -1,8 +1,89 @@
-public class Profile {
-    private String username;
-    //TODO
-    public Profile(String username) {
-        this.username = username;
+import java.util.ArrayList;
 
+/*
+ * TODO: all the setter methods and addFriend, removeFriend, block, unblock, etc. have to communicate with the client
+ *  and get info (the method parameters) from the user, also probably needs to be displayed on the GUI
+ */
+public class Profile {
+    private final String username;
+    private String password;
+    private String displayName;
+    private boolean receiveAll;
+    private ArrayList<Profile> friends;
+    private ArrayList<Profile> blocked;
+    //TODO
+    public Profile(String username, String password, String displayName, boolean receiveAll,
+                   ArrayList<Profile> friends, ArrayList<Profile> blocked) {
+        this.username = username;
+        this.password = password;
+        this.displayName = displayName;
+        this.receiveAll = receiveAll;
+        this.friends = friends;
+        this.blocked = blocked;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isReceiveAll() {
+        return receiveAll;
+    }
+
+    public void setReceiveAll(boolean receiveAll) {
+        this.receiveAll = receiveAll;
+    }
+
+    public ArrayList<Profile> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<Profile> friends) {
+        this.friends = friends;
+    }
+
+    public ArrayList<Profile> getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(ArrayList<Profile> blocked) {
+        this.blocked = blocked;
+    }
+
+    public boolean addFriend(Profile p) {
+        return friends.add(p);
+    }
+
+    public boolean removeFriend(Profile p) {
+        return friends.remove(p);
+    }
+
+    public boolean block(Profile p) {
+        return blocked.add(p);
+    }
+
+    public boolean unblock(Profile p) {
+        return blocked.remove(p);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Profile && ((Profile) o).getUsername().equals(username);
     }
 }
