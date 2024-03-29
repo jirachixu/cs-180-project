aa06f4df3ac182e6950b6c02700ce9c14b19aa2b
jirachixu <xu1838@purdue.edu>
2024-03-29 16:19:54 -0400

    edited DatabaseInterface to match the methods I wrote

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -7,7 +7,7 @@ public interface DatabaseInterface {
     public void sendMessage(Message message);
     public void editMessage(Message message, String newContent) throws MessageError;
     public void deleteMessage(Message message) throws MessageError;
-    public void createUser(String username, String password, String displayName);
-    public boolean deleteUser(String username);
-    public boolean editUser(String username, String newDisplayName);
+    public boolean createProfile(String username, String password);
+    public boolean deleteProfile(String username);
+    public boolean editDisplayName(String username, String newDisplayName);
 }
