906f3734bea4305c76b147a36183d84f5d8c0bd7
caasib <cboakye@purdue.edu>
2024-04-12 21:05:12 -0400

    edited ChatServer to work more ideally like the shown examples, removed ClientHandler

--- ChatServer.java
+++ ChatServer.java
@@ -3,7 +3,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ChatServer implements ServerInterface {
+public class ChatServer implements ServerInterface, Runnable {
+    private final Socket socket;
+    static Database database;
+
+    public ChatServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        ServerSocket serverSocket;
+        Socket clientSocket;
+        BufferedReader inFromUser;
+        PrintWriter outToUser;
+        ObjectOutputStream outToClient;
+        String fromUser;
+        try {
+            inFromUser = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outToUser = new PrintWriter(socket.getOutputStream());
+            fromUser = inFromUser.readLine();
+            while (fromUser != null) {
+                switch (fromUser) {
+                    case "createNewUser" -> {
+                        if (database.createProfile(inFromUser.readLine(), inFromUser.readLine(), inFromUser.readLine(),
+                                Boolean.parseBoolean(inFromUser.readLine()))) {
+                            outToUser.println("true");
+                            outToUser.flush();
+                        } else {
+                            outToUser.println("false");
+                            outToUser.flush();
+                        }
+                    }
+                    case "login" -> {
+                        if (database.login(inFromUser.readLine(), inFromUser.readLine())) {
+                            outToUser.println("true");
+                            outToUser.flush();
+                        } else {
+                            outToUser.println("false");
+                            outToUser.flush();
+                        }
+                    }
+                }
+                fromUser = inFromUser.readLine();
+            }
+            inFromUser.close();
+            outToUser.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the name of the file from which to read profiles: ");
@@ -15,72 +64,16 @@ public class ChatServer implements ServerInterface {
         System.out.println("Enter the name of the file to which to write chats: ");
         String chatsOut = sc.nextLine();
 
-        Database database = new Database(profileIn, chatsIn, profileOut, chatsOut);
+        database = new Database(profileIn, chatsIn, profileOut, chatsOut);
         database.readProfile();
         database.readChat();
 
         ServerSocket serverSocket;
-        Socket clientSocket;
-        BufferedReader inFromUser;
-        PrintWriter outToUser;
-        ObjectOutputStream outToClient;
         serverSocket = new ServerSocket(8080);
         while (true) {
-            try {
-                clientSocket = serverSocket.accept();
-                inFromUser = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                outToUser = new PrintWriter(clientSocket.getOutputStream());
-                outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
-
-                new Thread(new ClientHandler(clientSocket)).start();
-                String fromUser = inFromUser.readLine();
-
-                switch (fromUser) {
-                    case "createNewUser":
-                        String username;
-                        do {
-                            username = inFromUser.readLine();
-                        } while (database.getProfiles().containsKey(username));
-
-                        outToUser.println("true");
-                        outToUser.flush();
-
-                        String password = inFromUser.readLine();
-
-                        outToUser.println("true");
-                        outToUser.flush();
-
-                        String displayName = inFromUser.readLine();
-                        String receiveAll = inFromUser.readLine();
-
-                        if (database.createProfile(username, password, displayName,
-                                Boolean.parseBoolean(receiveAll))) {
-                            outToUser.println("true");
-                            outToUser.flush();
-                            break;
-                        }
-                        outToUser.println("false");
-                        outToUser.flush();
-                        break;
-
-                    case "login":
-                        String user = inFromUser.readLine();
-                        String pwd = inFromUser.readLine();
-
-                        if (database.login(user, pwd)) {
-                            outToClient.writeObject(database.getProfiles().get(user));
-                            outToClient.flush();
-                            break;
-                        }
-
-                        outToClient.writeObject(null);
-                        outToClient.flush();
-                        break;
-                }
-            } catch (IOException e) {
-                System.out.println("Error connecting to the server!");
-                return;
-            }
+            Socket socket = serverSocket.accept();
+            ChatServer server = new ChatServer(socket);
+            new Thread(server).start();
         }
     }
 }
